122d85dc89cf361d810efea8ff713b266c6d165c
endersonMarques <emmf@cin.ufpe.br>
2024-02-13 13:36:39 -0300

    chore: add satsohi family into android and ios fonts the native way

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
-import { Text } from './src/components/Text/Text';
+import {SafeAreaView} from 'react-native';
+import {Text} from './src/components/Text/Text';
 
 function App(): React.JSX.Element {
   return (
     <SafeAreaView>
-      <Text> Hello World!!</Text>
+      <Text preset="headingLarge"> Hello World!!</Text>
+      <Text style={{fontFamily: 'Satoshi-Light'}} preset="headingLarge">
+        {' '}
+        Hello World!!
+      </Text>
     </SafeAreaView>
   );
 }
